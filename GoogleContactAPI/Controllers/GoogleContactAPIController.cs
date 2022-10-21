bf5ab1952c130ed460eecf90b87660691692931c
SUPACHAIWAT <supachai.wat@gmail.com>
2022-10-21 18:28:29 +0700

    new project

---
+++ GoogleContactAPI/Controllers/GoogleContactAPIController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace GoogleContactAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class GoogleContactAPIController : ControllerBase
+	{
+		public GoogleContactAPIController()
+        {
+
+        }
+
+
+    }
+}
